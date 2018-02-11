128ef9f9e796096e3336fc2a6f41bd0debb974af
anhardeni <anhardeni@gmail.com>
2018-02-11 13:08:42 +0700

    versi 1.0

---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,18 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["lastchance/controllers:BankingController"] = append(beego.GlobalControllerRouter["lastchance/controllers:BankingController"],
+		beego.ControllerComments{
+			Method: "ShowAccounts",
+			Router: `/banking`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+}
---
+++ routers/router.go
@@ -0,0 +1,12 @@
+package routers
+
+import (
+	"lastchance/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+    beego.AutoRouter(&controllers.AccountController{})
+    beego.Include(&controllers.BankingController{})
+}
